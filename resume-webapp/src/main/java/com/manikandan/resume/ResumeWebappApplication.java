4b3ab95c2e23ae8df1c624ff47a8107f32c65e6e
Jaya Suriya <jayasuriya77721@gmail.com>
2026-09-18 22:56:46 +0530

    Add comment for extracurricular activities section
    Added a comment to indicate where to include extracurricular activities.

--- resume-webapp/src/main/java/com/manikandan/resume/ResumeWebappApplication.java
+++ resume-webapp/src/main/java/com/manikandan/resume/ResumeWebappApplication.java
@@ -1,8 +1,7 @@
 package com.manikandan.resume;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//add section about your extra curricular activities
 @SpringBootApplication
 public class ResumeWebappApplication {
 	public static void main(String[] args) {
